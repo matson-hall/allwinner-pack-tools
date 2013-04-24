c997661d4befd75c6f233dbb809cf74ce1b7211f
Zergmk2 <zergmk2@gmail.com>
2013-04-24 18:24:33 +0800

    add uboot relevant codes in pach-test.sh Signed-off-by: Zergmk2 <zergmk2@gmail.com>

--- pack-test.sh
+++ pack-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+set -x
 
 LINUX_ROOT=${PWD}/linux
+UBOOT_ROOT=${PWD}/u-boot
 BR_ROOT=${PWD}/buildroot
 TOOLS_ROOT=${PWD}/tools
 CROSS_COMPILE=arm-linux-gnueabihf-
@@ -10,45 +11,54 @@ OUT_ROOT=${PWD}/out
 
 build_prepare()
 {
-    rm -rf ${OUT_ROOT}
-    mkdir ${OUT_ROOT}
+	rm -rf ${OUT_ROOT}
+	mkdir ${OUT_ROOT}
+}
+
+build_uboot()
+{
+	cd ${UBOOT_ROOT}
+	make sun4i CROSS_COMPILE=arm-linux-gnueabihf-
+	cp ${UBOOT_ROOT}/u-boot.bin ${OUT_ROOT}/u-boot.bin
 }
 
 build_linux()
 {
-    cd ${LINUX_ROOT}
-    cp arch/arm/configs/cubieboard_defconfig .config
-    make ARCH=arm LOADADDR=0x40008000  -j4 uImage modules
+	cd ${LINUX_ROOT}
+	cp arch/arm/configs/cubieboard_defconfig .config
+	make ARCH=arm LOADADDR=0x40008000  -j4 uImage modules
+	./scripts/dtc/dtc -I dts -O dtb -o arch/arm/boot/dts/sun4i-a10-cubieboard.dtb arch/arm/boot/dts/sun4i-a10-cubieboard.dts
 	cp ${LINUX_ROOT}/arch/arm/boot/uImage ${OUT_ROOT}/boot.img
 	cp ${LINUX_ROOT}/arch/arm/boot/dts/sun4i-a10-cubieboard.dtb ${OUT_ROOT}/cb.dtb
 }
 
 build_br()
 {
-    cd ${BR_ROOT}
-    make O=${OUT_ROOT}/br cubieboard_defconfig
-    make O=${OUT_ROOT}/br LICHEE_GEN_ROOTFS=n
+	cd ${BR_ROOT}
+	make O=${OUT_ROOT}/br cubieboard_defconfig
+	make O=${OUT_ROOT}/br LICHEE_GEN_ROOTFS=n
 
-    echo "Regenerating rootfs"
-    
-    (
+	echo "Regenerating rootfs"
+
+	(
 	cd $LINUX_ROOT
 	make ARCH=arm modules_install INSTALL_MOD_PATH=${OUT_ROOT}/br/target
-    )
+	)
 
-    make O=${OUT_ROOT}/br target-generic-getty-busybox
-    make O=${OUT_ROOT}/br target-finalize
-    make O=${OUT_ROOT}/br LICHEE_GEN_ROOTFS=y rootfs-ext4
-    cp ${OUT_ROOT}/br/images/rootfs.ext4 ${OUT_ROOT}/
+	make O=${OUT_ROOT}/br target-generic-getty-busybox
+	make O=${OUT_ROOT}/br target-finalize
+	make O=${OUT_ROOT}/br LICHEE_GEN_ROOTFS=y rootfs-ext4
+	cp ${OUT_ROOT}/br/images/rootfs.ext4 ${OUT_ROOT}/
 }
 
 build_pack()
 {
-    cd ${PACK_ROOT}
-    ./pack -c sun4i -p linux -b cubieboard
+	cd ${PACK_ROOT}
+	./pack -c sun4i -p linux -b cubieboard
 }
 
-#build_prepare
+build_prepare
+build_uboot
 build_linux
 build_br
 build_pack
